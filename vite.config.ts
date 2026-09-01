08064fa879e8c28ad52ee8560a5b68afe18e8b95
sadeellive <sadeellive@gmail.com>
2026-09-02 01:23:37 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  preview: {
-    allowedHosts: ['billlessamerica.com', '.billlessamerica.com', true],
-  },
-  server: {
-    allowedHosts: ['billlessamerica.com', '.billlessamerica.com', true],
-  },
+  plugins: [react()],
 });
- 
